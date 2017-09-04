18e9218f2d79bcd0e089df4144a5ee346a53119f
Idan Asraf <idan.asraf@sap.com>
2017-09-04 13:28:10 +0300

    fixed controller setup

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ const bodyParser = require("body-parser");
 const proxy = require("express-http-proxy");
 
 const InfoController = require("./controller/info-ctrl");
-const DiscoveryController = require("./controller/cinema-ctrl");
 
 let app = new express();
 let port = process.env.PORT || 4000;
@@ -11,7 +10,6 @@ let port = process.env.PORT || 4000;
 app.use(bodyParser.json());
 
 app.use("/info", InfoController);
-app.use("/discovery", DiscoveryController);
 app.use("\/(movies|celebs|oscars)(\/.+)*", proxy(process.env.MOVIE_SERVICE_HOST));
 app.use("\/(cinemas|theaters|shows)(\/.+)*", proxy(process.env.SHOW_SERVICE_HOST));
 app.use("\/(orders|customers)(\/.+)*", proxy(process.env.ORDER_SERVICE_HOST));
