5feba8e94a29c6c8ea838cff0006354d2a6e062f
Idan Asraf <idan.asraf@sap.com>
2017-09-04 21:58:02 +0300

    dockerized

--- src/server.js
+++ src/server.js
@@ -7,12 +7,21 @@ const InfoController = require("./controller/info-ctrl");
 let app = new express();
 let port = process.env.PORT || 4000;
 
+const movieService = process.env.MOVIE_SERVICE_HOST;
+const showService = process.env.SHOW_SERVICE_HOST;
+const orderService = process.env.ORDER_SERVICE_HOST;
+
 app.use(bodyParser.json());
 
 app.use("/info", InfoController);
-app.use("\/(movies|celebs|oscars)(\/.+)*", proxy(process.env.MOVIE_SERVICE_HOST));
-app.use("\/(cinemas|theaters|shows)(\/.+)*", proxy(process.env.SHOW_SERVICE_HOST));
-app.use("\/(orders|customers)(\/.+)*", proxy(process.env.ORDER_SERVICE_HOST));
+app.use("/movies", proxy(movieService + "/movies"));
+app.use("/celebs", proxy(movieService + "/celebs"));
+app.use("/oscars", proxy(movieService + "/oscars"));
+app.use("/cinemas", proxy(showService + "/cinemas"));
+app.use("/theaters", proxy(showService + "/theaters"));
+app.use("/shows", proxy(showService + "/shows"));
+app.use("/orders", proxy(orderService + "/orders"));
+app.use("/customers", proxy(orderService + "/customers"));
 
 app.listen(port, () => {
    console.log(`Movie service is running on port ${port}`);
